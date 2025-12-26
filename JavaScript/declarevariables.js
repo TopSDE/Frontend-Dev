034438e78219e4d4bb794aca9c931133af1d5267
punithm5 <punithlokesh42@gmail.com>
2025-12-26 08:38:15 +0530

    26/12/25

--- JavaScript/declarevariables.js
+++ JavaScript/declarevariables.js
@@ -1,21 +1,25 @@
-// 1️⃣ var Features
-var x // declaration
-var x = 10 // declaration + initialization
-x = 20 // reinitialization
-var x // Redeclaration is ignored
-var x = 7 // redeclaration + reinitialization
+// 1️⃣var Features
+var x // declaration --> "undefined (no error)"      --> "YES"
+x = 10 // initialisation                             --> "YES"
+//var x = 10 // declaration + initialization         --> "YES"
+//x = 20 // reinitialization                         --> "YES"
+//var x // Redeclaration --> is ignored              --> "YES"
+//var x = 7 // redeclaration + reinitialization      --> "YES"
 console.log(x)
 
-// function scope = i can only access global scope
-function test() {
-    var a = 5
-}
 
-// console.log(a) // ❌ error (function scope)
+// scope of var = Func Scope : a variable is declared inside a function, 
+// I cannot access it outside the function.
+// If it is declared inside a block, I can access it outside the block.
+function test() {
+    var a = 77
+}
+//console.log(a) // ❌ error (function scope)
+
 
 // 👉 Hoisting means: JavaScript moves variable and 
 //    function declarations to the top of their scope before execution.
-console.log(c); // undefined (no error)
+//console.log(c); // "undefined (no error)"
 var c = 10;
 
     // What JavaScript understands internally of above code:
@@ -26,11 +30,60 @@ var c = 10;
     // Output => undefined
     // ⚠️ Only declarations are hoisted, not values.
 
-// 2️⃣ let Features
+// -------------------------------------------------------------------------------
 
-var y // declaration
-var y = 10 // declaration + initialization
-y = 20 // reinitialization
-var y // Redeclaration is ignored
-var y = 20 // redeclaration + reinitialization
-console.log(y)
+// 2️⃣let Features
+
+//let y // declaration --> "undefined (no error)"      --> "YES"
+//y = 8 // initialisation                              --> "YES"
+//let y = 10 // declaration + initialization           --> "YES"
+//y = 20 // reinitialization                           --> "YES"
+
+//let y // Redeclaration                               --> "NO"
+//let y = 30 // redeclaration + reinitialization       --> "NO"
+
+//console.log(y)
+
+// scope of let = Func Scope nd Block Scope: 
+// If a variable is declared inside a function OR inside a block, 
+// I cannot access it outside.
+function test() {
+    let a = 5
+    //const a = 10
+}
+//console.log(a)
+
+
+// Hoisting cant be done Here
+//console.log(b); // ❌ ReferenceError: Cannot access 'b' before initialization
+//let b = 10;
+
+// -------------------------------------------------------------------------------
+
+// 3️⃣const Features
+
+//const z // declaration                            --> "NO"
+//z = 10 // initialisation                          --> "NO"
+
+const z = 10 // declaration + initialization        --> "YES"
+
+//z = 20 // reinitialization                        --> "NO"
+//const z // Redeclaration                          --> "NO"
+//const z = 30 // redeclaration + reinitialization  --> "NO"
+
+//console.log(z)
+
+
+// scope of let = Func Scope nd Block Scope: 
+// If a variable is declared inside a function OR inside a block, 
+// I cannot access it outside.
+function test() {
+    //let a = 5
+    const a = 10
+}
+//console.log(a)
+
+
+// Hoisting cant be done Here
+//console.log(b); // ❌ ReferenceError: Cannot access 'b' before initialization
+//let b = 10;
